a7ab7783820f22a950a36c69102c8897f66569b1
Sireesha18 <116700405+DLSireesha@users.noreply.github.com>
2026-04-19 12:22:28 +0530

    Update msg-queue-tf.tf

--- msg-queue-tf.tf
+++ msg-queue-tf.tf
@@ -1,9 +1,7 @@
 # Creating a queue in test service in solace pubsub+
-# while initializing terraform init.. just keep only configuration file in the folder
-# execute terraform in ubuntu then go to folder.
-#if terraform not installed install with sudo snap install terraform
 
-#Provider is a plugin which will install required(solace) provide to create and manage resources.
+
+#Provider is a plugin which will install required provider(solace) to create and manage resources.
 terraform {
   required_providers {
     solacebroker = {
@@ -17,15 +15,15 @@ terraform {
 # Authentication the provider with user credentials
 provider "solacebroker" {
   # Event broker management user credentials
-  username = "test1-admin"
-  password = "dk7ia7jp275ipojjnc3n2pn6ch"
+  username = "###"
+  password = "#####"
   # The base URL of the Solace event broker,
   #  for example https://mybroker.example.org:<semp-service-port>
-  url      = "https://mr-connection-5n01mprnw8h.messaging.solace.cloud:943"  #don't add /SEMP/v2/config at the end
+  url  = "https://mr-connection-5n01mprnw8h.messaging.solace.cloud:943"  #don't add /SEMP/v2/config at the end
 }
 
 # Create a message queue on the event broker
-resource "solacebroker_msg_vpn_queue" "q" {
+resource "solacebroker_msg_vpn_queue" "queue" {
     msg_vpn_name = "test1"
     queue_name      = "GLOBAL.07332.2078.SERVICES"
     ingress_enabled = true
